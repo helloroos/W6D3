8e116e08052da459bc4580859f6114a61a0f4112
michelleroos <hello.michelleroos@gmail.com>
2021-04-01 11:27:11 -0400

    Worked on artwork_shares

--- first_routes_controllers/app/models/artwork.rb
+++ first_routes_controllers/app/models/artwork.rb
@@ -1,4 +1,18 @@
+# == Schema Information
+#
+# Table name: artworks
+#
+#  id        :bigint           not null, primary key
+#  title     :string           not null
+#  image_url :string           not null
+#  artist_id :integer          not null
+
 class Artwork < ApplicationRecord
 
     validates :title, presence: true, uniqueness: true
+
+    belongs_to :artist,
+        primary_key: :id,
+        foreign_key: :artist_id,
+        class_name: :User
 end
